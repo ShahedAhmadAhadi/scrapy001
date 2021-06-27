50cd55de23354d6f97966c7b0b57fa35ef43f742
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-27 23:39:18 +0430

    some experiments

--- mySpider.py
+++ mySpider.py
@@ -7,11 +7,14 @@ class SiteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for quote in response.css('div.quote'):
+        for quote in response.css('pre.prettyprint'):
             yield {
-                'author': quote.xpath('span/samll/text()').get(),
-                'text': quote.css('span.text::text').get(),
+                'all': quote.css('::text').get(),
             }
-        next_page = response.css('li.next a::attr("href")').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+        #     yield {
+        #         'author': quote.xpath('span/samll/text()').get(),
+        #         'text': quote.css('span.text::text').get(),
+        #     }
+        # next_page = response.css('li.next a::attr("href")').get()
+        # if next_page is not None:
+            # yield response.follow(next_page, self.parse)
---
+++ mysite.jl
@@ -0,0 +1,16 @@
+{"a": null}
+{"a": null}
+{"a": null}
+{"a": null}
+{"a": null}
+{"a": "<span class=\"meta nocode\"><b>HTTP 200 OK</b>\n<b>Allow:</b> <span class=\"lit\">GET, POST, HEAD, OPTIONS</span>\n<b>Content-Type:</b> <span class=\"lit\">application/json</span>\n<b>Vary:</b> <span class=\"lit\">Accept</span>\n\n</span>"}
+{"a": null, "b": null}
+{"a": "<span class=\"meta nocode\"><b>HTTP 200 OK</b>\n<b>Allow:</b> <span class=\"lit\">GET, POST, HEAD, OPTIONS</span>\n<b>Content-Type:</b> <span class=\"lit\">application/json</span>\n<b>Vary:</b> <span class=\"lit\">Accept</span>\n\n</span>", "b": null}
+{"text": null}
+{"text": "\n"}
+{"text": null}
+{"text": "\n"}
+{"text": null}
+{"text": "http://localhost:8000/car/2333"}
+{"all": "GET"}
+{"all": "HTTP 200 OK"}
